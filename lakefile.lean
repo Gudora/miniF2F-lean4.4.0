5c7ecf1aeff3fd25f79dd892f82581ecbfa8b4b7
Gudora <hujiewen02@gmail.com>
2024-02-25 03:07:59 -0500

    update to 4.5.0

--- lakefile.lean
+++ lakefile.lean
@@ -13,5 +13,9 @@ lean_lib «MiniF2F» {
   -- add library configuration options here
 }
 
-require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "master"
-require LeanCopilot from git "https://github.com/lean-dojo/LeanCopilot" @ "v1.1.1"
+-- require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "master"
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git" @ "feec58a7ee9185f92abddcf7631643b53181a7d3"
+-- require LeanCopilot from git "https://github.com/lean-dojo/LeanCopilot" @ "v1.1.1"
+require LeanCopilot from git "https://github.com/lean-dojo/LeanCopilot" @ "031f024e2a6440b789d0c74c6074a6263a456546"
